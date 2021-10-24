af771b365315dd89814ab9327c07dfd9a4d44038
Admin <147523689a@gmail.com>
2021-10-24 22:25:47 +0700

    first commit

---
+++ src/Entity/Student.java
@@ -0,0 +1,17 @@
+package Entity;
+
+public class Student {
+    private String studentID;
+    private String name;
+    private String gender;
+    private String birthday;
+    private String address;
+    private String classroom;
+    private String email;
+    private String phone;
+    private Float scoreGPA;
+    private Account account;
+
+
+
+}
