d54c45a2b8ac60638900307535ac67f679d6e4fb
lanhcrab <lanhpham301295@gmail.com>
2022-01-25 02:36:40 +0700

    update teacher

--- front-end/app/src/main/java/com/example/studentmanagement/teacher/Teacher_Activity.java
+++ front-end/app/src/main/java/com/example/studentmanagement/teacher/Teacher_Activity.java
@@ -12,15 +12,14 @@ public class Teacher_Activity extends AppCompatActivity {
 
     private TextView name;
     private Button btnExit;
-    private Button btnReg_Student, btn_Insert;
+    private Button btnReg_Student, btn_Insert, btn_List_Student, btn_Funds, btn_Event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_teacher);
 
-        name = findViewById(R.id.name_teacher);
-        btnExit = findViewById(R.id.btn_dangxuat);
+       initUI();
 
 
         if (!SharedPref.getInstance(this).isLoggedIn()) {
@@ -31,7 +30,7 @@ public class Teacher_Activity extends AppCompatActivity {
         String loggedName = SharedPref.getInstance(this).LoggedInUser();
         name.setText(loggedName);
 
-
+        //button logout
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,7 +39,7 @@ public class Teacher_Activity extends AppCompatActivity {
             }
         });
 
-        btnReg_Student = findViewById(R.id.reg_student);
+        //button register student
         btnReg_Student.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,6 +48,7 @@ public class Teacher_Activity extends AppCompatActivity {
             }
         });
 
+        // button insert student
         btn_Insert = findViewById(R.id.ínsert_student);
         btn_Insert.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,8 +57,39 @@ public class Teacher_Activity extends AppCompatActivity {
             }
         });
 
+        //button list student
+        btn_List_Student = findViewById(R.id.list_student);
+        btn_List_Student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), List_Student_Teacher_Activity.class));
+            }
+        });
 
+        btn_Funds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Funds_Teacher_Activity.class));
+            }
+        });
 
+        btn_Event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), List_Event_Teacher_Activity.class));
+            }
+        });
+    }
+
+    //anh xa
+    private void initUI() {
+
+        name = findViewById(R.id.name_teacher);
+
+        btnExit = findViewById(R.id.btn_dangxuat);
+        btnReg_Student = findViewById(R.id.reg_student);
+        btn_Funds = findViewById(R.id.funds);
+        btn_Event = findViewById(R.id.event);
     }
 
 
